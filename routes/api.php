cbdedc726bed2c6e9e4bf6db56f0800507ef7fae
Master Steve <mastersteve@Samurai.local>
2025-12-20 07:36:36 +0300

    Auth Route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//Auth Routes
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register')->action('api.auth.register');
+    Route::post('/login', 'login')->action('api.auth.login');
+    //Route::post('/logout', 'logout')->middleware('auth:sanctum');
+});
